3fca08365be0ff7d9df5470c0c599960032809c5
RICCOM <ericomun@gmail.comgit>
2024-07-04 00:08:18 +0300

    creating model, migrations and routes

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -14,13 +14,48 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.json
+    body = data.get('body')
+    username = data.get('username')
+
+    if not body or not username:
+        return make_response(jsonify({"error": "Body and username are required"}), 400)
+
+    new_message = Message(body=body, username=username)
+    db.session.add(new_message)
+    db.session.commit()
+    
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.json
+    body = data.get('body')
+
+    if not body:
+        return make_response(jsonify({"error": "Body is required"}), 400)
+
+    message.body = body
+    message.updated_at = datetime.utcnow()
+    db.session.commit()
+    
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
